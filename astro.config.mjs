0fa6ccfbd39a6961f5e976f10be60e0680668b5d
Isaac Marovitz <isaacryu@icloud.com>
2023-08-20 14:44:54 +0100

    More setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 import { defineConfig } from 'astro/config';
+import UnoCSS from "unocss/astro";
 import vue from "@astrojs/vue";
 
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), partytown()]
+  integrations: [
+    vue(), 
+    UnoCSS({
+      injectReset: true
+    }), 
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    })
+  ]
 });
