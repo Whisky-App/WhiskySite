9f5a4c44ef17f4ffda3729ea685f68472d228657
Isaac Marovitz <isaacryu@icloud.com>
2023-08-24 13:08:16 +0100

    Remove vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import UnoCSS from "unocss/astro";
-import vue from "@astrojs/vue";
 
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    vue(), 
     UnoCSS({
       injectReset: true
     }), 
