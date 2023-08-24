a58d359dcdbd599c2fef95d9df970d62cdb6d01a
Isaac Marovitz <isaacryu@icloud.com>
2023-08-24 13:14:57 +0100

    Remove partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
 import { defineConfig } from 'astro/config';
 import UnoCSS from "unocss/astro";
 
-import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     UnoCSS({
       injectReset: true
-    }), 
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
     })
   ]
 });
