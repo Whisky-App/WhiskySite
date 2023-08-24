e1ff28e0580d9f38a44da22d5391cf8387b8ef00
Isaac Marovitz <isaacryu@icloud.com>
2023-08-24 21:52:32 +0100

    Add Dutch

--- astro.i18n.config.ts
+++ astro.i18n.config.ts
@@ -2,7 +2,7 @@ import { defineAstroI18nConfig } from "astro-i18n"
 
 export default defineAstroI18nConfig({
 	defaultLangCode: "en",
-	supportedLangCodes: ["es", "fr", "it", "ja", "ko", "pt-BR", "pt", "zh-Hans", "zh-Hant"],
+	supportedLangCodes: ["es", "fr", "it", "ja", "ko", "nl", "pt-BR", "pt", "zh-Hans", "zh-Hant"],
 	showDefaultLangCode: false,
 	translations: {
 		"en": "src/translations/en.json",
@@ -11,6 +11,7 @@ export default defineAstroI18nConfig({
 		"it": "src/translations/it.json",
 		"ja": "src/translations/ja.json",
 		"ko": "src/translations/ko.json",
+		"nl": "src/translations/nl.json",
 		"pt-BR": "src/translations/pt-BR.json",
 		"pt": "src/translations/pt.json",
 		"zh-Hans": "src/translations/zh-Hans.json",
